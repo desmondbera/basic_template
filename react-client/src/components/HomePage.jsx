f4a8bbf3def21c2710e4d64660601d61897626d0
Desmond <desmond.bera@gmail.com>
2018-06-10 13:11:54 -0400

    Updating web template

---
+++ react-client/src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'React';
+
+class HomePage extends Component {
+
+  render() {
+    return (
+      <div>
+        Home page
+      </div>
+    )
+  }
+}
+
+export default HomePage;
