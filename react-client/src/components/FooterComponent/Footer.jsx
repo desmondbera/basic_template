81a0b1ba6ab9f319f5a5500a43903f7bab335580
Desmond <desmond.bera@gmail.com>
2018-06-14 13:27:14 -0400

    updated the footer

--- react-client/src/components/FooterComponent/Footer.jsx
+++ react-client/src/components/FooterComponent/Footer.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class Footer extends Component {
   render() {
     return (
       <footer>
-        <div>
-          Footer component
-        </div>
+        <ul id="footerButtons">
+          <li className="footerButton"><Link to=""><i class="fab fa-github"></i></Link></li>
+          <li className="footerButton"><Link to=""><i class="fab fa-medium"></i></Link></li>
+          <li className="footerButton"><Link to=""><i class="fab fa-linkedin"></i></Link></li>
+          <li className="footerButton"><Link to=""><i class="far fa-envelope"></i></Link></li>
+        </ul>
       </footer>
     )
   }
